ae7bd599cf9d83838fa21dbf9b4016c390600194
ZahraSafaei <zahrasafaei1272@gmail.com>
2025-10-21 11:12:38 +0330

    feature extraction & train

--- feature_extraction.py
+++ feature_extraction.py
@@ -17,56 +17,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 
 
-
-
-def augment_image(image, filename):
-    aug_images = []
-
-    # 1. Horizontal Flip
-    flipped = cv2.flip(image, 1)
-    aug_images.append(("flip", flipped))
-
-    # 2. Rotate by random angle (-25 to 25 degrees)
-    angle = random.randint(-25, 25)
-    rotated = transform.rotate(image, angle, mode='edge')
-    rotated = (rotated * 255).astype(np.uint8)
-    aug_images.append((f"rotate_{angle}", rotated))
-
-    # 3. Add random noise
-    noisy = util.random_noise(image)
-    noisy = (noisy * 255).astype(np.uint8)
-    aug_images.append(("noise", noisy))
-
-    # 4. Brightness adjustment
-    hsv = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
-    hsv[..., 2] = np.clip(hsv[..., 2] * 1.2, 0, 255)
-    brighter = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
-    aug_images.append(("bright", brighter))
-    
-    #5. main image
-    aug_images.append(("main", image))
-    
-    for aug_type, aug_img in aug_images:
-        save_path = os.path.join(aug_dir, f"{os.path.splitext(filename)[0]}_{aug_type}.jpg")
-        cv2.imwrite(save_path, cv2.cvtColor(aug_img, cv2.COLOR_RGB2BGR))
-
-    return [aug_img for _, aug_img in aug_images]
-
-
-
-
-#augment data and create an aug_images folder
-image_paths = glob.glob('C:/Users/ASUS/Desktop/resume_project/smile_detection/detection_with_LBP&HOG/genki4k/processed_images/*.jpg')
-aug_dir = 'C:/Users/ASUS/Desktop/resume_project/smile_detection/detection_with_LBP&HOG/genki4k/aug_images'
-os.makedirs(aug_dir, exist_ok=True)
-for path in image_paths:
-    #load every image and augment it 
-    img = cv2.imread(path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    filename = os.path.basename(path)
-    aug_images = augment_image(img, filename)
-
-
 def lbp_feature_vector(image, radius=3):
     # Convert to grayscale if needed
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) if len(image.shape) == 3 else image
@@ -187,7 +137,7 @@ def test(X_test, y_test, model_name = 'svm_model.joblib'):
 
 
 def main():
-    image_paths_new = natsorted(glob.glob('C:/Users/ASUS/Desktop/resume_project/smile_detection/detection_with_LBP&HOG/genki4k/aug_images/*.jpg'))
+    image_paths_new = natsorted(glob.glob('aug_images/*.jpg'))
     images = load_images(image_paths_new)
     X = extract_feature(images)
     y = prepare_label("labels.txt", 5)
